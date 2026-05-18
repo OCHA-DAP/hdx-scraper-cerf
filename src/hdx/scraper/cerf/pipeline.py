536fd2321179dcb9c10f9fe7db88de80e10036b0
Briar <briar.j.mills@gmail.com>
2026-05-18 12:49:03 -0400

    Write pipeline

---
+++ src/hdx/scraper/cerf/pipeline.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+"""CERF scraper"""
+
+import logging
+
+from hdx.api.configuration import Configuration
+from hdx.data.dataset import Dataset
+from hdx.data.resource import Resource
+from hdx.utilities.dictandlist import dict_of_lists_add
+from hdx.utilities.retriever import Retrieve
+from slugify import slugify
+
+logger = logging.getLogger(__name__)
+
+
+class Pipeline:
+    def __init__(self, configuration: Configuration, retriever: Retrieve, tempdir: str):
+        self._configuration = configuration
+        self._retriever = retriever
+        self._tempdir = tempdir
+        self.data = {}
+        self.dates = {}
+        self.headers = {}
+
+    def get_data(self):
+        for data_type in self._configuration["data_types"]:
+            source_url = self._configuration[data_type]["source_url"]
+            json = self._retriever.download_json(source_url)
+            nested_variables = self._configuration[data_type]["nested_variables"]
+            aggregation_method = self._configuration[data_type]["aggregation_method"]
+            for row in json:
+                new_row = {}
+                aggregate_row = {}
+                for key, value in row.items():
+                    if isinstance(value, dict):
+                        for _, subvalue in value.items():
+                            for subrow in subvalue:
+                                for subsubkey, subsubvalue in subrow.items():
+                                    if subsubkey not in nested_variables:
+                                        continue
+                                    aggregate_key = nested_variables[subsubkey]
+                                    self.add_header(aggregate_key, data_type)
+                                    dict_of_lists_add(
+                                        aggregate_row, aggregate_key, subsubvalue
+                                    )
+                    else:
+                        self.add_header(key, data_type)
+                        if key == "dateUSGSignature":
+                            value = value[:10]
+                            dict_of_lists_add(self.dates, data_type, value)
+                        new_row[key] = value
+                        if key == "latestDate":
+                            self.add_header("latestDateAsDate", data_type)
+                            new_row["latestDateAsDate"] = value[:10]
+                            dict_of_lists_add(self.dates, data_type, value[:10])
+                for key, value in aggregate_row.items():
+                    if aggregation_method == "list":
+                        new_row[key] = ", ".join(set(value))
+                    if aggregation_method == "sum":
+                        new_row[key] = sum(value)
+                dict_of_lists_add(self.data, data_type, new_row)
+        return
+
+    def generate_dataset(self, data_type: str) -> Dataset | None:
+        dataset_info = self._configuration[data_type]
+        dataset_title = dataset_info["title"]
+        dataset_name = slugify(dataset_title)
+        dataset = Dataset(
+            {
+                "name": dataset_name,
+                "title": dataset_title,
+                "notes": dataset_info["notes"],
+            }
+        )
+
+        dataset.set_time_period(min(self.dates[data_type]), max(self.dates[data_type]))
+        dataset.add_tags(self._configuration["tags"])
+        dataset.add_other_location("world")
+
+        # Add resources
+        resourcedata = {
+            "name": f"{dataset_title}.csv",
+            "description": "",
+        }
+        dataset.generate_resource(
+            self._tempdir,
+            f"{dataset_title}.csv",
+            self.data[data_type],
+            resourcedata,
+            self.headers[data_type],
+        )
+
+        resource = Resource(
+            {
+                "name": f"{dataset_title}.json",
+                "description": "",
+                "url": self._configuration[data_type]["source_url"],
+            }
+        )
+        resource.set_format("json")
+        dataset.add_update_resource(resource)
+        return dataset
+
+    def add_header(self, header: str, data_type: str) -> None:
+        if data_type not in self.headers:
+            self.headers[data_type] = []
+        if header not in self.headers[data_type]:
+            self.headers[data_type].append(header)
---
+++ tests/test_pipeline.py
@@ -0,0 +1,116 @@
+from os.path import join
+
+from hdx.utilities.compare import assert_files_same
+from hdx.utilities.downloader import Download
+from hdx.utilities.path import temp_dir
+from hdx.utilities.retriever import Retrieve
+
+from hdx.scraper.cerf.pipeline import Pipeline
+
+
+class TestPipeline:
+    def test_pipeline(self, configuration, fixtures_dir, input_dir, config_dir):
+        with temp_dir(
+            "TestCERF",
+            delete_on_success=True,
+            delete_on_failure=False,
+        ) as tempdir:
+            with Download(user_agent="test") as downloader:
+                retriever = Retrieve(
+                    downloader=downloader,
+                    fallback_dir=tempdir,
+                    saved_dir=input_dir,
+                    temp_dir=tempdir,
+                    save=False,
+                    use_saved=True,
+                )
+                pipeline = Pipeline(configuration, retriever, tempdir)
+                pipeline.get_data()
+
+                dataset = pipeline.generate_dataset("allocations")
+                dataset.update_from_yaml(
+                    path=join(config_dir, "hdx_dataset_static.yaml")
+                )
+                assert dataset == {
+                    "name": "cerf-allocations",
+                    "title": "CERF Allocations",
+                    "notes": "This dataset lists project funding allocations from OCHA's Central Emergency Response Fund (CERF). CERF allocations are made to ensure a rapid response to sudden-onset emergencies or to rapidly deteriorating conditions in an existing emergency and to support humanitarian response activities within an underfunded emergency.",
+                    "dataset_date": "[2006-03-15T00:00:00 TO 2026-10-24T23:59:59]",
+                    "tags": [
+                        {
+                            "name": "funding",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        }
+                    ],
+                    "groups": [{"name": "world"}],
+                    "license_id": "cc-by-igo",
+                    "methodology": "Registry",
+                    "caveats": "None",
+                    "dataset_source": "CERF",
+                    "package_creator": "HDX Data Systems Team",
+                    "private": False,
+                    "maintainer": "ccc6d05d-d2fd-45f9-be24-05c340968b4b",
+                    "owner_org": "cerf",
+                    "data_update_frequency": 7,
+                    "subnational": 0,
+                }
+                resources = dataset.get_resources()
+                assert resources == [
+                    {
+                        "name": "CERF Allocations.csv",
+                        "description": "",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "CERF Allocations.json",
+                        "description": "",
+                        "url": "http://cerfgms-webapi.unocha.org/v1/hdxproject/all.json",
+                        "format": "json",
+                    },
+                ]
+                file = "CERF Allocations.csv"
+                assert_files_same(join(fixtures_dir, file), join(tempdir, file))
+
+                dataset = pipeline.generate_dataset("donor-contributions")
+                dataset.update_from_yaml(
+                    path=join(config_dir, "hdx_dataset_static.yaml")
+                )
+                assert dataset == {
+                    "name": "cerf-donor-contributions",
+                    "title": "CERF Donor Contributions",
+                    "notes": "This dataset lists all contributions made by donors to the Central Emergency Response Fund (CERF). CERF receives broad support from United Nations Member States, observers, regional governments and international organizations, and the private sector, including corporations, non-governmental organizations and individuals.",
+                    "dataset_date": "[2015-07-03T00:00:00 TO 2026-05-12T23:59:59]",
+                    "tags": [
+                        {
+                            "name": "funding",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        }
+                    ],
+                    "groups": [{"name": "world"}],
+                    "license_id": "cc-by-igo",
+                    "methodology": "Registry",
+                    "caveats": "None",
+                    "dataset_source": "CERF",
+                    "package_creator": "HDX Data Systems Team",
+                    "private": False,
+                    "maintainer": "ccc6d05d-d2fd-45f9-be24-05c340968b4b",
+                    "owner_org": "cerf",
+                    "data_update_frequency": 7,
+                    "subnational": 0,
+                }
+                resources = dataset.get_resources()
+                assert resources == [
+                    {
+                        "name": "CERF Donor Contributions.csv",
+                        "description": "",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "CERF Donor Contributions.json",
+                        "description": "",
+                        "url": "http://cerfgms-webapi.unocha.org/v1/donorcontribution.json",
+                        "format": "json",
+                    },
+                ]
+                file = "CERF Donor Contributions.csv"
+                assert_files_same(join(fixtures_dir, file), join(tempdir, file))
